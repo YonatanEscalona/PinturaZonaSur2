6238c979d3b0b46e07ad265cccc4e7a77dca733c
Yonatan Escalona <y.escalona5@alumnos.santotomas.cl>
2025-08-21 23:02:42 -0400

    borre el servicios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 // tailwind.config.js
 module.exports = {
-    content: ["./*.html"],
+    content: ["./**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#1E40AF", // Azul personalizado
+                secondary: "#F59E0B", // Amarillo personalizado
+            },
+            fontFamily: {
+                sans: ["Inter", "sans-serif"],
+                serif: ["Merriweather", "serif"],
+            },
+        },
     },
     plugins: [],
 };
